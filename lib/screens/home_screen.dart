97aeccbe8e23637b6f5bf11784f4cc6a7ee83ca6
Mariano Sciacco <34033090+Maxelweb@users.noreply.github.com>
2020-11-27 12:39:14 +0100

    Starting the tutorial - title inserted

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,8 +6,22 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // List<IconData> _icons = [
+  //   FontAwesomeIcons.plane
+  // ]
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: SafeArea(
+            child: ListView(
+                padding: EdgeInsets.symmetric(vertical: 30.0),
+                children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(left: 20.0, right: 120.0),
+            child: Text('Cosa vorresti cercare?',
+                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+          )
+        ])));
   }
 }
